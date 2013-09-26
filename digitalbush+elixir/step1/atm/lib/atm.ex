9533ab1f22f36341892a475de5b571246479852d
Josh Bush <josh@digitalbush.com>
2013-09-25 23:11:51 -0500

    Adding client API

--- digitalbush+elixir/step1/atm/lib/atm.ex
+++ digitalbush+elixir/step1/atm/lib/atm.ex
@@ -4,4 +4,16 @@ defmodule Atm do
   def start(_type, state) do
     Atm.Supervisor.start_link(state)
   end
+
+  def check_balance(account_number) do
+  	:gen_server.call(:atm,{:check_balance,account_number})
+  end
+
+  def withdraw(account_number, amount) do
+  	:gen_server.call(:atm,{:withdraw,account_number,amount})
+  end
+
+  def deposit(account_number, amount) do
+  	:gen_server.call(:atm,{:deposit,account_number,amount})
+  end
 end
